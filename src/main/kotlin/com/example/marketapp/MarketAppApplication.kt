14a179868606212412ac1da9dcba6984ef2f1ad1
AmadeAli1 <aliamade29@gmail.com>
2022-08-05 14:22:02 +0200

    Properties

--- src/main/kotlin/com/example/marketapp/MarketAppApplication.kt
+++ src/main/kotlin/com/example/marketapp/MarketAppApplication.kt
@@ -1,22 +1,14 @@
 package com.example.marketapp
 
-import com.example.marketapp.repository.AccountRepository
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.runBlocking
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MarketAppApplication(val repository: AccountRepository) : CommandLineRunner {
+class MarketAppApplication : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        runBlocking {
-            repository.findAll().collect{
-                println(it)
-            }
-        }
+
     }
 }
 
