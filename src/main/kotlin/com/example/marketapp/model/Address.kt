a9399b24edf67529e41688252b7df569463b6345
AmadeAli1 <aliamade29@gmail.com>
2022-08-19 19:46:45 +0200

    LocationService.kt

--- src/main/kotlin/com/example/marketapp/model/Address.kt
+++ src/main/kotlin/com/example/marketapp/model/Address.kt
@@ -5,12 +5,13 @@ import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.util.*
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 @Table("address")
 data class Address(
     @field:NotBlank @Column("name") var addressName: String,
-    @field:NotBlank var latitude: Double,
-    @field:NotBlank var longitude: Double,
+    @field:NotNull var latitude: Double,
+    @field:NotNull var longitude: Double,
     @field:NotBlank @Column("userid") val userId: UUID,
 ) {
     @Id
